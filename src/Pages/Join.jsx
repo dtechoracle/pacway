4e3599662cd9ee5b195ca92d49b9d74a193aa714
dtechoracle <iamezekieljeremiah@gmail.com>
2024-04-13 00:52:33 -0700

    edited the job page

--- src/Pages/Join.jsx
+++ src/Pages/Join.jsx
@@ -6,7 +6,7 @@ const Join = () => {
   return (
     <div>
       <Navbar />
-      <div className="p-24">
+      <div className="md:p-24 p-10">
         <h1 className="font-black text-6xl mb-4">Join us</h1>
         <p>
           We are a team that has worked together for several years but, when it
@@ -20,15 +20,17 @@ const Join = () => {
           single original game so far!
         </p>
 
-        <div className="mt-12">
+        <div className="md:mt-12 mt-24">
           <h1 className="font-black text-6xl">Roles</h1>
-          <div className="mt-4">
+          <div className="md:mt-4 mt-8">
             <div className="flex mb-6">
               <div className="w-1/2">
                 <h1 className="font-bold">VFX Artist</h1>
               </div>
               <div className="w-1/2">
-                <h1 className="text-blue-700 font-bold">Apply Here</h1>
+                <h1 className="text-blue-700 font-bold items-end">
+                  Apply Here
+                </h1>
               </div>
             </div>
             <div className="flex mb-6">
