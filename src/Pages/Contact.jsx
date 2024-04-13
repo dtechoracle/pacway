e72c8017beef284dc1753a9b150c8cc590265077
dtechoracle <iamezekieljeremiah@gmail.com>
2024-04-13 01:12:40 -0700

    made the contact us page mobile responsive

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -6,9 +6,9 @@ const Contact = () => {
   return (
     <div>
       <Navbar />
-      <div className="p-24">
+      <div className="md:p-24 p-12">
         <h1 className="font-black text-6xl mb-4">Contact us</h1>
-        <p className="w-[667px]">
+        <p className="md:w-[667px]">
           We are a team that has worked together for several years but, when it
           comes to our own games, we are really young, with Moonlighter as our
           single original game so far!
@@ -19,19 +19,19 @@ const Contact = () => {
             <input
               type="text"
               placeholder="Name"
-              className="w-[80%] mb-6 border-b-2 border-gray h-24 text-6xl"
+              className="md:w-[80%] w-full mb-6 border-b-2 border-gray h-24 text-6xl"
             />
             <br />
             <input
               type="email"
               placeholder="Email"
-              className="w-[80%] mb-6 border-b-2 border-gray h-24 text-6xl"
+              className="md:w-[80%] w-full mb-6 border-b-2 border-gray h-24 text-6xl"
             />
             <br />
             <input
               type="text"
               placeholder="AIM"
-              className="w-[80%] mb-6 border-b-2 border-gray h-24 text-6xl"
+              className="md:w-[80%] w-full mb-6 border-b-2 border-gray h-24 text-6xl"
             />
           </form>
           <button className="mt-8 bg-blue-700 p-6 font-bold text-4xl rounded text-white w-[300px]">
