5d71ace5ebe023404a8d70d0d9c61f0f299f6212
dtechoracle <iamezekieljeremiah@gmail.com>
2024-04-13 00:59:38 -0700

    made the games page mobile responsive

--- src/Pages/Games.jsx
+++ src/Pages/Games.jsx
@@ -10,12 +10,12 @@ const Games = () => {
         <div className="relative">
           <img
             src="/img/games1.png"
-            className="h-[534px] w-auto"
+            className="md:h-[534px] h-[300px] w-auto"
             alt="Game 1"
           ></img>
           <div className="absolute inset-0 bg-black bg-opacity-50">
             <div className="absolute bottom-0 left-0 text-white p-16">
-              <h1 className="font-black text-8xl">SORUNNE III</h1>
+              <h1 className="font-black md:text-8xl">SORUNNE III</h1>
               <button className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                 View Game
               </button>
@@ -25,12 +25,12 @@ const Games = () => {
         <div className="relative">
           <img
             src="/img/games2.png"
-            className="h-[534px] w-auto"
+            className="md:h-[534px] h-[300px] w-auto"
             alt="Game 2"
           ></img>
           <div className="absolute inset-0 bg-black bg-opacity-50">
             <div className="absolute bottom-0 left-0 text-white p-16">
-              <h1 className="font-black text-8xl">LIGHTFALL</h1>
+              <h1 className="font-black md:text-8xl">LIGHTFALL</h1>
               <button className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                 View Game
               </button>
@@ -40,12 +40,12 @@ const Games = () => {
         <div className="relative">
           <img
             src="/img/games3.png"
-            className="h-[534px] w-auto"
+            className="md:h-[534px] h-[300px] w-auto"
             alt="Game 3"
           ></img>
           <div className="absolute inset-0 bg-black bg-opacity-50">
             <div className="absolute bottom-0 left-0 text-white p-16">
-              <h1 className="font-black text-8xl">DAYLIGHT</h1>
+              <h1 className="font-black md:text-8xl">DAYLIGHT</h1>
               <button className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                 View Game
               </button>
